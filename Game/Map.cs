b283bab4d34c2bbf96e3283396e3a1ad992855f9
HrEJAC <hcv743@alumni.ku.dk>
2018-03-17 21:01:04 +0100

    Sequence converted to interface

--- Game/Map.cs
+++ Game/Map.cs
@@ -1,10 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Game
 {
     public class Map
     {
+        List<Sequence> CurrentSeq;
+
         public Map()
         {
+            CurrentSeq = new List<Sequence> { };
+
+        }
+
+        private Sequence CreateRandSeq(){
+            var rand = new Random();
+
+            switch (rand.Next(5)){
+                case 1:
+                    var seq = new aSequence();
+                    seq.
+                    return aSequence();
+                case 2:
+                    return aSequence();
+                case 3:
+                    return aSequence();
+                case 4:
+                    return aSequence();
+                case 5:
+                    return aSequence();
+            }
+        }
+
+        public void Update(float step){
+            foreach (Sequence seq in CurrentSeq){
+                seq.Move(step);
+            }
+
         }
     }
 }
---
+++ Game/SplitSequence.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game
+{
+    public class SplitSequence : ISequence
+    {
+        public List<Obstacle> Container;
+        public float PosX;
+        public float length;
+
+        public SplitSequence(float startX)
+        {
+            PosX = startX;
+            Container = new List<Obstacle> { };
+        }
+
+        public void CreateObstacle(
+            float relX, float width, float Y, float height, string img)
+        {
+            Container.Add(new Obstacle(
+                            new Shape(PosX + relX, Y - height,
+                                      width, height), img));
+        }
+
+        public void CreateObstacles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(float step)
+        {
+            PosX -= step;
+            foreach (Obstacle obs in Container)
+            {
+                obs.shape.PosX -= step;
+            }
+        }
+    }
+}
