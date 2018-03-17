617f9bed19cf0df7b2c4268716de75a54d07505d
Marc Pedersen <BareMarcP@Marcs-MacBook-Air.local>
2018-03-17 20:13:09 +0100

    Shape og tests

--- Game/Shape.cs
+++ Game/Shape.cs
@@ -3,8 +3,72 @@ namespace Game
 {
     public class Shape
     {
-        public Shape()
+        public float PosX;
+        public float PosY;
+        public float ExtendX;
+        public float ExtendY;
+
+        public float Speed;
+
+        public float Angle;
+        public float AngFreq;
+
+        public Shape(float posX, float posY, float extendX, float extendY)
         {
+            PosX = posX;
+            PosY = posY;
+            ExtendX = extendX;
+            ExtendY = extendY;
+        }
+
+        public Shape(float posX, float posY, float extendX,
+                     float extendY, float speed, float angle,
+                     float angFreq){
+            PosX = posX;
+            PosY = posY;
+            ExtendX = extendX;
+            ExtendY = extendY;
+            Speed = speed;
+            Angle = angle;
+            AngFreq = angFreq;
+        }
+
+        public static bool Collision (Shape player, Shape obstacle) {
+            
+            var PlayerLeftX = player.PosX; 
+            var PlayerLowerY = player.PosY;
+            var PlayerRightX = player.PosX + player.ExtendX;
+            var PlayerUpperY = player.PosY + player.ExtendY;
+
+            var ObstacleLeftX = obstacle.PosX;
+            var ObstacleLowerY = obstacle.PosY;
+            var ObstacleRightX = obstacle.PosX + obstacle.ExtendX;
+            var ObstacleUpperY = obstacle.PosY + obstacle.ExtendY;
+
+            if ((PlayerRightX >= ObstacleLeftX &&
+                 PlayerRightX <= ObstacleRightX &&
+                 PlayerUpperY >= ObstacleLowerY &&
+                 PlayerUpperY <= ObstacleUpperY) ||
+                
+                (PlayerRightX >= ObstacleLeftX &&
+                 PlayerRightX <= ObstacleRightX &&
+                 PlayerLowerY <= ObstacleUpperY &&
+                 PlayerLowerY >= ObstacleLowerY) ||
+               
+                (PlayerLeftX >= ObstacleLeftX &&
+                 PlayerLeftX <= ObstacleRightX &&
+                 PlayerUpperY >= ObstacleLowerY &&
+                 PlayerUpperY <= ObstacleUpperY) ||
+               
+                (PlayerLeftX >= ObstacleLeftX &&
+                 PlayerLeftX <= ObstacleRightX &&
+                 PlayerLowerY <= ObstacleUpperY &&
+                 PlayerLowerY >= ObstacleLowerY))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
---
+++ GameTests/Test.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using System;
+using Game;
+
+namespace GameTests
+{
+    [TestFixture()]
+    public class Test
+    {
+        private Shape Player;
+        private Shape Obstacle;
+
+        [Test()]
+        public void TestNoCollision()
+        {
+            Player = new Shape(0.0f, 0.0f, 0.1f, 0.1f);
+            Obstacle = new Shape(1.0f, 1.0f, 0.1f, 0.1f);
+            Assert.IsFalse(Shape.Collision(Player, Obstacle));
+        }
+
+        [Test()]
+        public void TestCollisionPlayerLowerLeftCorner()
+        {
+            Player = new Shape(0.5f, 0.5f, 1.0f, 1.0f);
+            Obstacle = new Shape(0.0f, 0.0f, 1.0f, 1.0f);
+            Assert.IsTrue(Shape.Collision(Player, Obstacle));
+        }
+
+        [Test()]
+        public void TestCollisionPlayerLowerRightCorner()
+        {
+            Player = new Shape(0.0f, 0.5f, 1.0f, 1.0f);
+            Obstacle = new Shape(0.5f, 0.0f, 1.0f, 1.0f);
+            Assert.IsTrue(Shape.Collision(Player, Obstacle));
+        }
+
+        [Test()]
+        public void TestCollisionPlayerUpperRightCorner()
+        {
+            Player = new Shape(0.0f, 0.0f, 1.0f, 1.0f);
+            Obstacle = new Shape(0.5f, 0.5f, 1.0f, 1.0f);
+            Assert.IsTrue(Shape.Collision(Player, Obstacle));
+        }
+
+        [Test()]
+        public void TestCollisionPlayerUpperLeftCorner()
+        {
+            Player = new Shape(0.5f, 0.0f, 1.0f, 1.0f);
+            Obstacle = new Shape(0.0f, 0.5f, 1.0f, 1.0f);
+            Assert.IsTrue(Shape.Collision(Player, Obstacle));
+        }
+        // Fikses hvis det er problem
+        [Test()]
+        public void TestCollisionPlayerstabinmiddle()
+        {
+            Player = new Shape(0.4f, 0.4f, 1.5f, 1.5f);
+            Obstacle = new Shape(0.5f, 0.5f, 1.0f, 1.0f);
+            Assert.IsFalse(Shape.Collision(Player, Obstacle));
+        }
+
+    }
+}
